6090e5796d4d2c65cdf5eee75d3488c27a53f4c6
ginohmk <michotall95@gmail.com>
2022-05-17 15:03:17 +0100

    Add Crud as specified Delete Create Read Except update

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', sign_up: 'register' }
 
   resources :users, only: %i[ index show] do
-    resources :recipes
-    resources :foods
+    resources :foods, except: %i[update]
+    resources :recipes, only: %i[ index show] 
   end
 
   root 'users#index'
