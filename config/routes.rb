e223db45ea942ef2b9bd655ce1510c0425173653
ginohmk <michotall95@gmail.com>
2022-05-17 08:42:05 +0100

    Modify Route and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  # devise_for :users
+
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', sign_up: 'register' }
 
-  resources :users do
+  resources :users, only: %i[ index show] do
     resources :recipes
     resources :foods
   end
