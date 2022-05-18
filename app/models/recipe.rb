df82e9b460c8abf1f00ccb9e2c0076773b428fd3
ginohmk <michotall95@gmail.com>
2022-05-18 16:50:13 +0100

    Fix linters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_food
 
-
   def self.recipe_update_public(recipe_id)
     recipe = Recipe.find(recipe_id)
     recipe.update(public: !recipe.public)
